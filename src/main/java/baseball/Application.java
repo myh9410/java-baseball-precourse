d5dd0effba0be4b7e02e8fa91915f731ddb6a13e
myh941 <mjskks94@naver.com>
2022-10-03 03:11:56 +0900

    feat : 기능 요구사항
    3자리 숫자에 대한 기능 개발
    - 제공된 랜덤 유틸을 사용한 3자리의 수 생성
    - 3자리의 수 중복된값 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        createValidRandomNumber();
+    }
+
+    /**
+     * 정답값을 생성한다.
+     * @return string
+     */
+    private static String createValidRandomNumber() {
+        String answer = Integer.toString(Randoms.pickNumberInRange(1, 9));
+        answer += Integer.toString(Randoms.pickNumberInRange(1, 9));
+        answer += Integer.toString(Randoms.pickNumberInRange(1, 9));
+
+        if (!checkUniqueNumber(answer)) createValidRandomNumber();
+
+        return answer;
+    }
+
+    /**
+     * 서로 다른 수로 이루어진 3자리인지 체크한다.
+     * @param answer 정답값
+     * @return boolean
+     */
+    private static boolean checkUniqueNumber(String answer) {
+        return answer.charAt(0) != answer.charAt(1) &&
+                answer.charAt(0) != answer.charAt(2) &&
+                answer.charAt(1) != answer.charAt(2);
     }
 }
