5e91b11f1ae9aa4db9b55686972ae35a96d45af2
myh9410 <mjskks94@naver.com>
2022-10-03 03:18:53 +0900

    feat : 기능 요구사항
    스트라이크,볼,낫싱 결과에 대한 기능 개발
    - 사용자로부터 입력값을 받는다.
    - 사용자 입력값에 대한 유효성검증을 한다.
    - 입력값을 이용하여 스트라이크, 볼, 낫싱, 정답 여부를 확인한다.
    - 정답이 아닌경우, 게임을 계속 진행한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,82 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        createValidRandomNumber();
+        String answer = createValidRandomNumber();
+        playBaseBallGame(answer);
+    }
+
+    /**
+     * 야구게임을 진행하고, 정답을 못맞추면 계속 진행, 정답을 맞추면 재시작 혹은 종료한다.
+     * @param answer 정답값
+     */
+    private static void playBaseBallGame(String answer) {
+        String userInput = getUserNumberInput();
+        String inputResult = checkAnswer(answer, userInput);
+
+        System.out.println(inputResult);
+
+        if (!inputResult.equals("3스트라이크")) {
+            playBaseBallGame(answer);
+            return;
+        }
+    }
+
+    /**
+     * 스트라이크, 볼, 낫싱, 정답 여부를 판단한다.
+     * @param answer 정답값
+     * @param userInput 사용자 입력값
+     * @return string
+     */
+    private static String checkAnswer(String answer, String userInput) {
+        StringBuilder hintBuilder = new StringBuilder();
+
+        int[] strikeAndBall = new int[2];
+
+        if (answer.charAt(0) == userInput.charAt(0)) strikeAndBall[0]++;
+        if (answer.charAt(0) == userInput.charAt(1) || answer.charAt(0) == userInput.charAt(2)) strikeAndBall[1]++;
+
+        if (answer.charAt(1) == userInput.charAt(1)) strikeAndBall[0]++;
+        if (answer.charAt(1) == userInput.charAt(0) || answer.charAt(1) == userInput.charAt(2)) strikeAndBall[1]++;
+
+        if (answer.charAt(2) == userInput.charAt(2)) strikeAndBall[0]++;
+        if (answer.charAt(2) == userInput.charAt(0) || answer.charAt(2) == userInput.charAt(1)) strikeAndBall[1]++;
+
+        int strike = strikeAndBall[0];
+        int ball = strikeAndBall[1];
+
+        if (strike == 0 && ball == 0) return "낫싱";
+        if (ball > 0) hintBuilder.append(ball).append("볼");
+        if (hintBuilder.length() > 0) hintBuilder.append(" ");
+        if (strike > 0) hintBuilder.append(strike).append("스트라이크");
+
+        return hintBuilder.toString();
+    }
+
+    /**
+     * 사용자로부터 숫자값을 입력받는다.
+     * @return string
+     */
+    private static String getUserNumberInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        String userInput = Console.readLine();
+        numberInputValidation(userInput);
+
+        return userInput;
+    }
+
+    /**
+     * 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 내려준다.
+     * @param userInput 사용자 입력값
+     */
+    private static void numberInputValidation(String userInput) {
+        String numericAndLength3 = "^[0-9]{3}$";
+
+        if (!userInput.matches(numericAndLength3)) throw new IllegalArgumentException();
     }
 
     /**
